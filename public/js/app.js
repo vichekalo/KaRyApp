115f1e445635ae4287e1feee13d9fb2ceafae6ee
channarypha <channary.pha@student.passerellesnumeriques.org>
2021-05-20 13:30:19 +0700

    3

---
+++ public/js/app.js
@@ -0,0 +1,50 @@
+// --------------------------------***********  LOG IN **********------------------------------
+function displayUser(respone){
+    console.log(respone.data);
+    let users = respone.data;
+    let userList = document.querySelector('.user-list');
+    let ul = document.querySelector('ul');
+    if (ul !== null){
+        ul.remove();
+    }
+    const newUl = document.createElement('ul');
+    for (let user of users){
+        const li = document.createElement('li');
+        li.textContent = "userid: " + user.id + " " + user.username + " " + user.password;
+        newUl.appendChild(li);
+        userList.appendChild(newUl);
+    }
+}
+
+function save(event){
+    const username = document.querySelector("#username").value;
+    const password = document.querySelector("#password").value;
+
+
+    let user = {username: username, password: password}
+    const url = "http://localhost:5000/api/users";
+    axios
+    .post(url, user)
+    .then(displayUser)
+}
+
+function deleteUser(){
+    
+}
+
+function updateUser(){
+
+}
+
+function loadData(){
+    const url = "http://localhost:5000/api/users";
+    axios
+    .get()
+    .then(displayUser)
+}
+
+const saveUser = document.querySelector("#save");
+saveUser.addEventListener('click', save);
+loadData();
+
+
--- server.js
+++ server.js
@@ -1,10 +1,84 @@
+// LOGIN
+const { urlencoded } = require('body-parser');
 const express = require('express');
 const app = express();
 
-app.listen(process.env.PORT || 5000, () => console.log("Server running..."));
-
+app.listen(process.env.PORT || 5000, () => console.log("server runnig"))
+app.use(express.static('public'));
 app.use(express.json());
 app.use(express.urlencoded());
-app.use(express.static('public'));
 
-app.get('/', (req, res) => res.send("Hello Project"))
+
+let users = [{
+    username: "kaka",
+    password: "1"
+}, {
+    username: "nary",
+    password: "12"
+}, {
+    username: "rady",
+    password: "123"
+}];
+
+app.get('/api/users', (req, res) => res.send(users));
+
+app.post('/api/users', (req, res) => {
+    console.log(req.body);
+    if (!req.body.password) {
+
+        res.status(400)
+        return res.send({ error: "Password Required" })
+    }
+    let user = {
+        id: users.length + 1,
+        username: req.body.username,
+        password: req.body.password,
+    };
+    users.push(user);
+    res.send(users);
+});
+// --------------- delete ------------------
+app.delete('/api/users/:id', (req, res) => {
+    let id = req.params.id;
+    console.log(username, password)
+    let index = -1;
+
+    for (let user of users){
+        if (user.id === parseInt(id)){
+            index = user.id - 1;
+        }
+    }
+    if (index >= 0){
+        let user = users[index];
+        users.splice(index, 1);
+        res.send(user);
+    }else{
+        res.status(404);
+        res.send({error: " not valid user id"})
+    }
+})
+
+// ----------------------- put --------------------------------
+app.put('/api/users/:id', (req, res) => {
+    let id = req.params.id;
+    let username = req.body.username;
+    let password = req.body.password;
+    console.log(username, password)
+    let index = -1;
+
+    for (let user of users){
+        if (user.id=== parseInt(id)){
+            index = user.id - 1;
+        }
+    }
+    if (index >= 0){
+        let user = users[index];
+        user.username = username;
+        user.password = password;
+        users.splice(index, 1);
+        res.send(user);
+    }else{
+        res.status(404);
+        res.send({error: " not valid user id"})
+    }
+})
