97b26a5b0d56b2f689fbff261545fa017e7c5f1d
channarypha <channary.pha@student.passerellesnumeriques.org>
2021-05-21 08:52:51 +0700

    2

--- server.js
+++ server.js
@@ -1,37 +1,5 @@
 // ------------------***********************  LOGIN  **********************----------------------------------
-// TODO: Create a server.
-<<<<<<< HEAD
-const fs = require('fs');
-const chat=(JSON.parse(fs.readFileSync('')))
-// const express=require('express');
-// const app=express();
-// // code will work with port 5000
-// app.listen(process.env.PORT || 5000,()=>console.log('server running'))
 
-// app.use(express.json());
-// app.use(express.urlencoded())
-// //list of users and password for login.
-// let users=[
-//     {username:"VichVich",password:"0101"},
-//     {username:"Nary ",password:"0202"},
-//     {username:"Kun",password:"168"},
-// ];
-// // app.get('/login',(req,res)=>res.send(users));
-// app.get("/login",(req,res)=>{
-//     // to get the username and password from the query of the request.
-//     let Username=req.query.username;
-//     let Password=req.query.password;
-//     let condition=false;
-//     // Check user and password if valid return true otherwise return false.
-//     for (let user of users){
-//         if (user.username===Username && user.password===Password){
-//             condition=true;
-//         }
-//     };
-//     res.send(condition);
-// });
-// app.use(express.static('public')); 
-=======
 const express = require('express');
 const app = express();
 
@@ -63,4 +31,4 @@ app.get("/login", (req,res) => {
     res.send(condition);
 });
 app.use(express.static('public')); 
->>>>>>> ef15516c6f288eb2d10ad622c4c9718d0af92bdd
+
