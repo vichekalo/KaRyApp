cab1f9a30ef3961ddd7875ff66c194ce5a556524
channarypha <channary.pha@student.passerellesnumeriques.org>
2021-05-21 16:47:22 +0700

    ry

--- public/js/app.js
+++ public/js/app.js
@@ -1,25 +1,18 @@
+const fs= require('fs');
+
 //show when we click on login bnt
-function showlogin(){
-    let x=document.querySelector('.feature3');
-    if (window.getComputedStyle(x).display==="none"){
-        x.style.display='block'
-    }
-}
-let btnlogin=document.querySelector('.log');
-btnlogin.addEventListener('click',showlogin);
-
-
-
-
-
-
-
-
-
+// function showlogin(){
+//     let x = document.querySelector('.feature3');
+//     if (window.getComputedStyle(x).display === "none"){
+//         x.style.display = 'block'
+//     }
+// }
+// let btnlogin = document.querySelector('.log');
+// btnlogin.addEventListener('click', showlogin);
 
 // function login(e){
 //     e.preventDefault();
-//     ////
+    ////
 //     let url="http://localhost:5000/login";
 //     axios
 //     .get(url)
@@ -42,3 +35,57 @@ btnlogin.addEventListener('click',showlogin);
 // let passWord = document.querySelector('#password');
 // let btnLogin = document.querySelector("#login");
 // btnLogin.addEventListener('click',login);
+
+// fruit
+
+let fruitList = [];
+
+function addFruit(fruitName,color,price){
+    let dic = {}
+    dic.name = fruitName;
+    dic.color = color;
+    dic.price = price;
+    fruitList.push(dic);
+    console.log("fruit added:"+dic.name);
+}
+
+function getColorOf(fruitName){
+    for( dic of fruitList){
+        if(dic.name === fruitName){
+            return dic.color;
+        }
+    }
+    return null
+}
+function getPriceOf(fruitName){
+    for( dic of fruitList){
+        if(dic.name === fruitName){
+            return dic.price;
+        }
+    }
+    return null
+}
+
+
+function listFruits(){
+    let result = "Here is the fruit: \n";
+    for (let dic of fruitList){
+        console.log(dic.name+"\n");
+    }
+}
+
+let save =() =>{
+    fs.writeFileSync("users.json",JSON.stringify(fruitList));
+}
+let load =() =>{
+    fruitList=JSON.parse(fs.readFileSync('users.json'));
+}
+
+module.exports ={
+    getColorOf,
+    getPriceOf,
+    load
+}
+
+
+// {"users":[{"id":1,"first_name":"Adam","last_name":"Mini","favorite_color":"#ccc","username":"admin","password":"admin"},{"id":2,"first_name":"Sora","last_name":"KH","favorite_color":"#eee","username":"sorakh","password":"sorakh"}],"messages":[{"id":1,"user_id":1,"msg":"Hello World","user":{"id":1,"first_name":"Adam","last_name":"Mini","favorite_color":"#ccc","username":"admin","password":"admin"}},{"id":2,"user_id":1,"msg":"admin","user":{"id":1,"first_name":"Adam","last_name":"Mini","favorite_color":"#ccc","username":"admin","password":"admin"}},{"id":3,"user_id":2,"msg":"admin","user":{"id":2,"first_name":"Sora","last_name":"KH","favorite_color":"#eee","username":"sorakh","password":"sorakh"}},{"id":4,"user_id":2,"msg":"This is sorakh","user":{"id":2,"first_name":"Sora","last_name":"KH","favorite_color":"#eee","username":"sorakh","password":"sorakh"}}]}
--- server.js
+++ server.js
@@ -1,34 +1,41 @@
 // ------------------***********************  LOGIN  **********************----------------------------------
-
-const express = require('express');
-const app = express();
+// const express = require('express');
+// const app = express();
 
 // code will work with port 5000
-app.listen(process.env.PORT || 5000,() => console.log('server running'))
+// app.listen(process.env.PORT || 5000,() => console.log('server running'))
+// app.use(express.json());
+// app.use(express.urlencoded())
 
-app.use(express.json());
-app.use(express.urlencoded())
 //list of users and password for login.
-let users = [
-    {username:"VichVich",password:"0101"},
-    {username:"Nary ",password:"0202"},
-    {username:"Kun",password:"168"},
-];
+// let users = [
+//     {username:"VichVich",password:"0101"},
+//     {username:"Nary ",password:"0202"},
+//     {username:"Kun",password:"168"},
+// ];
+
 // app.get('/login',(req,res)=>res.send(users));
-app.get("/login", (req,res) => {
+
+// app.get("/login", (req, res) => {
 
     // to get the username and password from the query of the request.
-    let Username = req.query.username;
-    let Password = req.query.password;
-    const condition = false;
+    // let Username = req.query.username;
+    // let Password = req.query.password;
+    // const condition = false;
 
     // Check user and password if valid return true otherwise return false.
-    for (let user of users){
-        if (user.username === Username && user.password === Password){
-            condition = true;
-        }
-    }
-    res.send(condition);
-});
-app.use(express.static('public')); 
+//     for (let user of users){
+//         if (user.username === Username && user.password === Password){
+//             condition = true;
+//         }
+//     }
+//     res.send(condition);
+// });
+// app.use(express.static('public')); 
+
+// 
+const{getColorOf, getPriceOf, load} = require('./app')
+load();
+console.log("mango color is :" + getColorOf("mango"));
+console.log("banana price is :" + getPriceOf("banana")); 
 
